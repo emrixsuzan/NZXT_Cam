357e4b50d88a3cb3370046e7da376953e5f741a2
emrixsuzan <86382039+emrixsuzan@users.noreply.github.com>
2024-02-21 18:46:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-
-import { MonitoringData } from "@nzxt/web-integrations-types/v1";
-
-const { height, width, targetFps, shape } = window.nzxt.v1;
-
-window.nzxt = {
-  v1: {
-    onMonitoringDataUpdate: (data: MonitoringData) => {
-      const { cpus, gpus, ram, kraken } = data;
-
-      // build cool visualizations with this data!
-    }
-  }
+alert("Hallo Welt");
 };
